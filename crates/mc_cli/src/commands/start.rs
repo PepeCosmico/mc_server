bd3fb636b4dee86bf4868413907fd3554a9cf73f
PedroLlinasFerrer <p.llinas@alumnos.upm.es>
2026-01-25 16:15:24 +0100

    conflict errors #23

--- crates/mc_cli/src/commands/start.rs
+++ crates/mc_cli/src/commands/start.rs
@@ -1,16 +1,16 @@
 use crate::{client::TcpClient, error::Result, utils::run_task};
-use mc_daemon::protocol::{TcpRequest, TcpResponse};
+use mc_daemon::protocol::{ResponsePayload, TcpRequest, TcpResponse};
 
 pub async fn run(address: &str) -> Result<()> {
     let client = TcpClient::new(address);
 
-    let response: TcpResponse<String> = run_task(
+    let response: TcpResponse = run_task(
         "Starting server...",
         "Server started successfully",
         "Error starting server",
         client.send_request(TcpRequest::Start),
     )
-    .await?;
+        .await?;
 
     if response.success {
         match response.data {
@@ -20,7 +20,7 @@ pub async fn run(address: &str) -> Result<()> {
             _ => tracing::error!("Server returned invalid response."),
         }
     } else {
-        tracing::error!("Error: {}", response.message.unwrap_or_default());
+        tracing::error!("Error: {:?}", response);
     }
 
     Ok(())
